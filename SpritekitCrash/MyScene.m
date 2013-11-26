bb2c1d126f760feccc87f1e2a8083a07b4528452
Sabatino Masala <Sabatino@Sabatinos-MacBook-Pro.local>
2013-11-26 23:33:41 +0100

    initial commit

--- SpritekitCrash/MyScene.m
+++ SpritekitCrash/MyScene.m
@@ -12,38 +12,10 @@
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        self.arrSounds = [[NSMutableDictionary alloc] init];
+        [self.arrSounds setObject:[SKAction playSoundFileNamed:[NSString stringWithFormat:@"70.caf  "] waitForCompletion:NO] forKey:[NSString stringWithFormat:@"1"]];
     }
     return self;
-}
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
 }
 
 -(void)update:(CFTimeInterval)currentTime {
